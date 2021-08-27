4d4ac4e1093f2053d6d9f3b1befc017bb326e7c3
giangtran <giang08t3@gmail.com>
2021-08-27 13:59:19 +0700

    update exercise if-else && switch-case

---
+++ week 2/condition/leapYear/inputYear.js
@@ -0,0 +1,32 @@
+function inputYear()
+{
+    let year = parseInt(document.forms["checkYear"]["year"].value);
+
+    let leapYear = false;
+
+    if (year % 4 == 0)
+    {
+        if (year % 100 == 0)
+        {
+            if (year % 400 == 0)
+            {
+                document.getElementById("leapYear").innerHTML = year +  " is leap year";
+            }
+            else
+            {
+                document.getElementById("leapYear").innerHTML = year +  " is  not leap year";
+            }
+        }
+        else
+        {
+            //alert('is leap year');
+            document.getElementById("leapYear").innerHTML = year +  " is leap year";
+        }
+    }
+    else 
+    {
+        // alert("is not leap year");
+        // console.log('leapYear');
+        document.getElementById("leapYear").innerHTML = year +  " is  not leap year";
+    }
+}
